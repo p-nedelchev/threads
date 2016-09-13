12a36b8411ab475aa146814f2750ee5b67e1987e
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-09-13 17:23:55 +0300

    Third task from Threads

--- src/main/java/com/clouway/third/Counter.java
+++ src/main/java/com/clouway/third/Counter.java
@@ -1,7 +1,41 @@
 package com.clouway.third;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Petar Nedelchev <peter.krasimirov@gmail.com>
  */
-public class Counter {
+public class Counter implements Runnable{
+    private int end;
+    private int counter;
+    private Object lock;
+
+
+
+    public Counter(int end, Object lock) {
+        this.end = end;
+        this.counter = 0;
+        this.lock = lock;
+    }
+
+    public void run() {
+        while (counter < end) {
+            synchronized (lock) {
+                counter++;
+                System.out.println(Thread.currentThread().getName() + " :" + counter);
+                lock.notifyAll();
+                try {
+                    if (counter < end) {
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
 }
--- src/main/java/com/clouway/third/Demo.java
+++ src/main/java/com/clouway/third/Demo.java
@@ -4,4 +4,14 @@ package com.clouway.third;
  * @author Petar Nedelchev <peter.krasimirov@gmail.com>
  */
 public class Demo {
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Counter counter = new Counter(8, lock);
+        Counter counter1 = new Counter(8, lock);
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter1);
+        thread1.start();
+        thread2.start();
+    }
+
 }
